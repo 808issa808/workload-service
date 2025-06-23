dc2ce0ce8bd204b611f08c10319063da4ae8d7d9
real_issa <isanur0202@gmail.com>
2025-06-23 17:35:42 +0500

    according to comment

--- src/main/java/org/epam/msg/KafkaConsumerConfig.java
+++ src/main/java/org/epam/msg/KafkaConsumerConfig.java
@@ -3,6 +3,7 @@ package org.epam.msg;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -16,20 +17,27 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+    @Value("${spring.kafka.consumer.properties.spring.json.trusted.packages}")
+    private String trustedPackages;
+    @Value("${spring.kafka.consumer.properties.spring.json.type.mapping}")
+    private String typeMapping;
+
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "workload");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                org.springframework.kafka.support.serializer.JsonDeserializer.class);
-        props.put("spring.json.trusted.packages", "org.epam.web.dto");
-        props.put("spring.json.type.mapping", "updateReport:org.epam.web.dto.UpdateReport");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonDeserializer.class);
+        props.put("spring.json.trusted.packages", trustedPackages);
+        props.put("spring.json.type.mapping", typeMapping);
 
         return new DefaultKafkaConsumerFactory<>(props);
     }
-
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
